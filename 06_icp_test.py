97da9b6540d1c24b8caae1944c21d578d8e88762
jerrywiston <dreamfantasy0@gmail.com>
2022-02-14 22:46:36 +0800

    complete icp test.

--- 06_icp_test.py
+++ 06_icp_test.py
@@ -2,6 +2,7 @@ from re import L
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import time
 
 from Slam.utils import EndPoint
 from Simulation.utils import State, ControlState
@@ -30,8 +31,7 @@ if __name__ == "__main__":
     sdata1 = info["lidar"].copy()
     img1 = cv2.flip(simulator.render(), 0)
     cv2.imshow("render1", img1)
-    state1 = simulator.state
-    pts_list1 = EndPoint(simulator.state.pose(), lidar_params, sdata1, True)
+    pts_list1 = EndPoint((0,0,0), lidar_params, sdata1, True)
     pts_list1 = np.array(pts_list1)
     plt.figure()
     plt.axis("equal")
@@ -39,31 +39,36 @@ if __name__ == "__main__":
     
     # Step 2
     for i in range(5):
-        state_next, info = simulator.step(ControlState("basic", 50, 10))
+        state_next, info = simulator.step(ControlState("basic", 20, 45))
         print(simulator.state)
     sdata2 = info["lidar"].copy()
     img2 = cv2.flip(simulator.render(), 0)
     cv2.imshow("render2", img2)
 
     #pts_list2 = EndPoint(simulator.state.pose(), lidar_params, sdata2, True)
-    pts_list2 = EndPoint(state1.pose(), lidar_params, sdata2, True)
+    pts_list2 = EndPoint((0,0,0), lidar_params, sdata2, True)
     pts_list2 = np.array(pts_list2)
     #plt.figure()
     plt.axis("equal")
     plt.plot(pts_list2[:,0], pts_list2[:,1], "go")
 
     # Iterative Closest Points
-    R, T = Icp(100, pts_list1, pts_list2)
-    print(R,T)
     pts_origin = EndPoint((0,0,0), lidar_params, sdata1, True)
     pts_origin = np.array(pts_origin)
     pts_align = EndPoint((0,0,0), lidar_params, sdata2, True)
-    pts_align = Transform(pts_align, R, T)
     pts_align = np.array(pts_align)
+    print(pts_origin, pts_align)
+    start = time.time()
+    R, T = Icp(30, pts_origin, pts_align)
+    end = time.time()
+    print(R,T)
+    print(end-start)
+    
+    pts_align_trans = Transform(pts_align, R, T)
     plt.figure()
     plt.axis("equal")
     plt.plot(pts_origin[:,0], pts_origin[:,1], "bo")
-    plt.plot(pts_align[:,0], pts_align[:,1], "go")
+    plt.plot(pts_align_trans[:,0], pts_align_trans[:,1], "go")
     
     cv2.waitKey(0)
     plt.show()
